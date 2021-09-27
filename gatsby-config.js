730d21c3d6bbfb2ac712ec63f53efc82c4c07848
Clay Harmon <clay@clayharmon.me>
2021-09-27 11:07:56 +0300

    Squashed commit of the following:
    commit 72dc0a218e4ae1a83f06f0596e836612a7651ba5
    Author: Clay Harmon <clay@clayharmon.me>
    Date:   Mon Sep 27 11:06:26 2021 +0300
    
        Add i18n plugins to gatsby and generate .po files
    
    commit 1ffb76488e1229bfc94d650b7d003394d681fadd
    Author: Clay Harmon <clay@clayharmon.me>
    Date:   Mon Sep 27 11:04:32 2021 +0300
    
        Setup i18n config files
    
    commit df069f33122397000c9a4e4177af156d136bb858
    Author: Clay Harmon <clay@clayharmon.me>
    Date:   Mon Sep 27 11:01:42 2021 +0300
    
        Add required i18n packages: lingui and i18n theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
     siteUrl: "https://lalemerkezi.com/",
   },
   plugins: [
+    {
+      resolve: "gatsby-theme-i18n",
+      options: {
+        defaultLang: "en",
+        prefixDefault: true,
+        locales: "en tr",
+        configPath: require.resolve("./i18n/config.json"),
+      },
+    },
+    {
+      resolve: "gatsby-theme-i18n-lingui",
+      options: {
+        localeDir: "./i18n/lingui",
+      },
+    },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
     {
